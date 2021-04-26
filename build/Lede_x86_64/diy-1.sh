c06f605c32fa0ca753a1a32209f1de99d0f8cf2a
mxj1668 <82012199+mxj1668@users.noreply.github.com>
2021-04-26 21:54:46 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -7,10 +7,11 @@
 # 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
 # 软件包地址：https://github.com/281677160/openwrt-package
 # 拉取插件请看《各种命令的简单介绍》第4条、第5条说明,不管大神还是新手请认真的看看,再次强调请不要一下子就拉取别人一堆插件的插件包,容易造成编译错误的
-git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
-
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus                                             #SSR出国 
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash                                         #openclash出国软件
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-openclash                                #argon主题
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom         #透明主题
 
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
