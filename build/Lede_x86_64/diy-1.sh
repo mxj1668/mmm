d769ff0b25ca91e80f6836cdd12ead19f23c593a
mxj1668 <82012199+mxj1668@users.noreply.github.com>
2021-04-27 18:23:45 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -13,7 +13,7 @@ git clone https://github.com/vernesong/OpenClash package/luci-app-openclash     
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom                           #透明主题
 #git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon                                                     #argon主题
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon  #删除lean主题安装新的argon主题
-rm -rf ./package/lean/luci-theme-netgear                                                                                              #删除lean主题安装新的netgear主题
+rm -rf ./package/lean/luci-theme-netgear                                                                                              #删除netgear主题
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config                                            #argon主题设置
 #全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
 #用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
