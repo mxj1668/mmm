39c7f91dda6fcbea1db890d6cae1e064fa4f6d9c
mxj1668 <82012199+mxj1668@users.noreply.github.com>
2021-04-30 22:24:32 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -2,6 +2,14 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
+sed -i "/uci commit fstab/a\uci commit network" $ZZZ
+sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.2.2'" $ZZZ                # IPv4 地址(openwrt后台地址)
+sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ             # IPv4 子网掩码
+sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.2.1'" $ZZZ               # IPv4 网关
+sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.2.255'" $ZZZ           # IPv4 广播
+sed -i "/uci commit network/i\uci set network.lan.dns='223.5.5.5 114.114.114.114'" $ZZZ     # DNS(多个DNS要用空格分开)
+sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                        # 去掉LAN口使用内置的 IPv6 管理
+
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
 
